28fff25fe7ef6eea346616402441898a69d9365f
Sasa Skeledzija <sasa.skeledzija@evia.de>
2021-08-22 17:14:44 +0200

    - Entity methods names refactoring - search for user entities by user id added

--- src/main/kotlin/com/bytelogics/webookserver/entities/ActivityField.kt
+++ src/main/kotlin/com/bytelogics/webookserver/entities/ActivityField.kt
@@ -2,6 +2,7 @@ package com.bytelogics.webookserver.entities
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.mapping.DBRef
 import org.springframework.data.mongodb.core.mapping.Document
 import java.util.*
 
@@ -9,6 +10,7 @@ import java.util.*
 data class ActivityField(@Id val id: String = UUID.randomUUID().toString(),
                          @Indexed(unique=true)
                          val name: String,
+                         @DBRef val user: User?,    // user can define own activity field
                          val description : String,
                          val rate: Int
 )
--- src/main/kotlin/com/bytelogics/webookserver/entities/OrganizationType.kt
+++ src/main/kotlin/com/bytelogics/webookserver/entities/OrganizationType.kt
@@ -2,6 +2,7 @@ package com.bytelogics.webookserver.entities
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.mapping.DBRef
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.MongoId
 import java.util.*
@@ -10,5 +11,6 @@ import java.util.*
 data class OrganizationType(@Id val id: String = UUID.randomUUID().toString(),
                             @Indexed(unique=true)
                             val name: String,
+                            @DBRef val user: User?,    // user can define own organization type
                             val description: String,
                             val rate: Int)
